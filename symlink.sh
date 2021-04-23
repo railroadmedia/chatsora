95aa58f3e979c6f5beae0f2cc77b21e1a95d2691
calebfavor <calebfavor@gmail.com>
2021-04-23 18:12:00 +0000

    New build

---
+++ symlink.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Ask the user which app they want to Symlink with
+echo Which application would you like to create a symlink for?
+options=("drumeo" "pianote" "guitareo")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "drumeo")
+            echo "Symlinking Chatsora with Drumeo!"
+            npm install; npm lib:watch & npm link; cd /app/drumeo/laravel; npm link @musora/chatsora; npm watch;
+            echo "Watching Drumeo For Changes..."
+            ;;
+        "pianote")
+            echo "Symlinking Chatsora with Pianote!"
+            npm install; npm lib:watch & npm link; cd /app/pianote; npm link @musora/chatsora; npm watch;
+            echo "Watching Pianote For Changes..."
+            ;;
+        "guitareo")
+            echo "Symlinking Chatsora with Guitareo!"
+            npm install; npm lib:watch & npm link; cd /app/guitareo; npm link @musora/chatsora; npm watch;
+            ;;
+        *)
+          echo "Invalid option $REPLY"
+          break
+          ;;
+    esac
+done
+
