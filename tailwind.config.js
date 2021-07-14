929b74d5dc261a7bfc535d5a3b27a38b2ba0420a
calebfavor <calebfavor@gmail.com>
2021-07-14 18:38:41 +0000

    replace tw- prefix with t- to fix conflicts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  prefix: 'tw-',
+  prefix: 't-',
   // important: true, // to be removed after other css frameworks are removed
   darkMode: false, // or 'media' or 'class'
   theme: {
